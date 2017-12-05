c428ee8775f7f5a24eeb1e55134b867d39f1754e
lishuaitj@126.com <lishuaitj@126.com>
2017-12-04 23:49:07 -0800

    opa_18a_login ok

--- ex1/l1.py
+++ ex1/l1.py
@@ -63,38 +63,21 @@ url = restPROTOCOL+'://'+restHOST
 #print restHOST
 #print
 
-cmd = '/rest/bug??bug_status=Open&bug_status=Working&bug_status=Rejected&bug_status=Fixed&bug_status=Verify&classification=IO_Options&classification=Internal_Storage&classification=Storage_Controllers&f1=version&f3=OP&f4=priority&f5=bug_severity&f6=CP&f7=cf_tagging&j3=OR&list_id=1738927&n7=1&o1=anywordssubstr&o4=equals&o5=equals&o7=equals&query_format=advanced&v1=Purley_MS%20Purley_Value%20Purley_Ventura%20Purley_Electron%20Purley_Stark%20imm3.cable%20imm3.cyborg%20imm3.carnage%20imm3.constantine%20imm3.electron%20imm3.stark%20imm3.ventura%2017A_Block%2017A_Mobile%20scom_mp_7.1%20scvmm_addin_3.1.0%20Purley_Proton%20%20Purley_Winterfell%20%20Purley_Odin%20%20imm3.proton%20%20imm3.winterfell%20%20imm3.odin%20%20imm3.dunbar%20%20purley_ga%20%20Purley_LXCA%20%20OneCli-Purley&v4=1&v5=Emergency&v7=P1%20Purley%20and%2017A%20FV%20Defects'
-#cmd = '/rest/bug??f1=keywords&f2=OP&f3=keywords&f4=reporter&f5=CP&list_id=714210&o1=substring&o3=substring&o4=substring&query_format=advanced&v1=DeferClone&v3=DeferClone&v4=bugzilla_automation'
-#cmd = '/rest/bug??f1=bug_status&f2=OP&f3=bug_status&f4=cf_limitation_status&f5=blocked&f6=cf_target_release&f7=CP&o1=equals&o3=equals&o4=substring&o5=isempty&o6=notequals&order=bug_id&query_format=advanced&v1=Limitation&v3=Limitation&v4=Temporary&v6=n%2Fa%20-%20Legacy%20defect'
-#cmd = '/rest/product?type=accessible'
-#cmd = '/rest/group?names=Lenovo'
-#cmd = '/editproducts.cgi?action=editgroupcontrols&product=Ridgeis_Istanbul'
-#cmd = '/rest/bug??f1=cf_dup_id&f2=OP&f3=cf_dup_id&limit=0&list_id=1048444&o1=isnotempty&o3=isnotempty&order=cf_answer%2Cbug_id%20DESC&query_format=advanced'
-#cmd = '/rest/bug??f1=bug_status&f10=bug_status&f11=OP&f12=cf_fixed_root_cause&f13=cf_solution&f14=CP&f16=CP&f18=CP&f2=OP&f3=cf_platform_affected&f4=cf_blocking_defect&f5=OP&f6=bug_status&f7=cf_build_fixed&f8=CP&f9=OP&j11=OR&j2=OR&o1=anywordssubstr&o10=anywordssubstr&o12=isempty&o13=isempty&o3=isempty&o4=equals&o6=equals&o7=isempty&query_format=advanced&resolution=---&v1=Open%20Working%20Rejected%20Fixed%20Verify&v10=Fixed%20Verify&v4=---&v6=Verify'
-#cmd = '/rest/bug??f1=version&f3=OP&f4=version&list_id=1112571&o1=changedto&o4=changedto&query_format=advanced&v1=_Field_Defect&v4=_Field_Defect'
+#cmd = '/rest/bug??bug_status=Open&bug_status=Working&bug_status=Rejected&bug_status=Fixed&bug_status=Verify&classification=IO_Options&classification=Internal_Storage&classification=Storage_Controllers&f1=version&f3=OP&f4=priority&f5=bug_severity&f6=CP&f7=cf_tagging&j3=OR&list_id=1738927&n7=1&o1=anywordssubstr&o4=equals&o5=equals&o7=equals&query_format=advanced&v1=Purley_MS%20Purley_Value%20Purley_Ventura%20Purley_Electron%20Purley_Stark%20imm3.cable%20imm3.cyborg%20imm3.carnage%20imm3.constantine%20imm3.electron%20imm3.stark%20imm3.ventura%2017A_Block%2017A_Mobile%20scom_mp_7.1%20scvmm_addin_3.1.0%20Purley_Proton%20%20Purley_Winterfell%20%20Purley_Odin%20%20imm3.proton%20%20imm3.winterfell%20%20imm3.odin%20%20imm3.dunbar%20%20purley_ga%20%20Purley_LXCA%20%20OneCli-Purley&v4=1&v5=Emergency&v7=P1%20Purley%20and%2017A%20FV%20Defects'
 
-# dups
-#cmd = '/rest/bug?&f1=cf_answer&f3=OP&f4=cf_answer&f5=resolution&f6=CP&limit=0&o1=equals&o4=equals&o5=equals&order=tag%2Ccf_target_release%2Cproduct%2Cbug_status%2Cpriority%2Cbug_severity&query_format=advanced&v1=Duplicate&v4=Duplicate&v5=DUPLICATE'
 
-#cmd = '/rest/bug??f1=bug_status&f2=OP&f3=bug_status&f4=days_elapsed&list_id=1048141&o1=equals&o3=equals&o4=lessthan&query_format=advanced&v1=Limitation&v3=Limitation&v4=45'
+#cmd = '/buglist.cgi?bug_status=Open&bug_status=Working&bug_status=Fixed&bug_status=Verify&classification=IO_Options&component=Intel_OmniPath&list_id=3247878&product=Intel_OmniPath&query_format=advanced&resolution=---&version=18A_Block'
 
-#bug_id = 38673
-#bug_id = 38598
-#bug_id = 73649
-#bug_id = str(sys.argv[1])
-#cmd = '/rest/bug'
-#cmd = cmd + '/'+ str(bug_id) 
-# if history is desired 
-# cmd = cmd + '/history' 
+cmd = '/rest/bug??bug_status=Open&bug_status=Working&bug_status=Fixed&bug_status=Verify&classification=IO_Options&component=Intel_OmniPath&list_id=3247878&product=Intel_OmniPath&query_format=advanced&resolution=---&version=18A_Block&ctype=csv&human=1'
+
 
 uri = url + cmd
 
-#print 
 print uri
 
 r = requests.get(uri, params={'api_key': api_key},verify=False)
 
-#print r.text
+print r.text
 #print r.status_code
 #exit
 D = r.json()
@@ -107,7 +90,7 @@ D = r.json()
 
 
 for defect in D['bugs']:
-    print json.dumps(defect, sort_keys=True, indent=2, separators=(',', ': '))
+   # print json.dumps(defect, sort_keys=True, indent=2, separators=(',', ': '))
     bug_id = defect['id']
     headline = defect['summary']
     product = defect['product']
