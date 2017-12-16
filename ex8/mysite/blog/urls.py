debfe9b577565c077f0fcc121ef6f849fc174bfa
Hans2009 <lishuaitj@126.com>
2017-12-16 05:06:13 -0800

    chapter one: publish first article

--- ex8/mysite/blog/urls.py
+++ ex8/mysite/blog/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.blog_title,name="blog_title"),
+    url(r'^content$',views.blog_article,name="blog_article"),
 ]
