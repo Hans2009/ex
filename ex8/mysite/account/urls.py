483650f32c3906d3196fb9052d75ef6b52e519f1
Hans2009 <lishuaitj@126.com>
2017-12-17 01:48:34 -0800

    login by template_name

--- ex8/mysite/account/urls.py
+++ ex8/mysite/account/urls.py
@@ -6,5 +6,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     #url(r'^login/$',views.user_login,name="user_login"),
-    url(r'^login/$', auth_views.login,name="user_login"),
+    #url(r'^login/$', auth_views.login,name="user_login"),
+    url(r'^new_login/$', auth_views.login, {"template_name": "account/login.html"}),
 ]
