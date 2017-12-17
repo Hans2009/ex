d31a3d5e43db9c2e2f55599f2d3d24fd74f38d2e
Hans2009 <lishuaitj@126.com>
2017-12-16 19:12:41 -0800

    login successfully

--- ex8/mysite/mysite/urls.py
+++ ex8/mysite/mysite/urls.py
@@ -20,5 +20,6 @@ from . import view
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls',namespace='blog',app_name='blog')),
+    url(r'^account/', include('account.urls',namespace='account',app_name='account')),
     url(r'^home$',view.home_page), 
 ]
